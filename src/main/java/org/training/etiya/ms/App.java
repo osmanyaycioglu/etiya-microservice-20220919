32ec2564dbed8a935c61ff6c3697c07b81605e02
osman <osman@osman123.com>
2022-09-20 14:48:42 +0300

    Initial commit

---
+++ src/main/java/org/training/etiya/ms/App.java
@@ -0,0 +1,13 @@
+package org.training.etiya.ms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
